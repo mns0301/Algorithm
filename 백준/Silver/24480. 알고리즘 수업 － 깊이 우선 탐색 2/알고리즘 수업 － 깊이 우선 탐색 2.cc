9e117d420fa01675200bcffec4cd477399c5c6dc
mns0301 <unitymns@gmail.com>
2024-08-06 14:29:15 +0900

    [Silver II] Title: 알고리즘 수업 - 깊이 우선 탐색 2, Time: 76 ms, Memory: 14048 KB -BaekjoonHub

---
+++ 백준/Silver/24480. 알고리즘 수업 － 깊이 우선 탐색 2/알고리즘 수업 － 깊이 우선 탐색 2.cc
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <stdlib.h>
+using namespace std;
+
+void DFS(vector<int> v[], int visit[], int r, int* cnt) {
+	if (visit[r] == 0) {
+		visit[r] = (*cnt)++;
+		for (int i = 0; i < v[r].size(); i++) DFS(v, visit, v[r][i], cnt);
+	}
+	return;
+}
+
+bool compare(int a, int b) {
+	return a > b;
+}
+
+int main(void) {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	cout.tie(0);
+
+
+	int n, m, r;
+	cin >> n >> m >> r;
+	vector<int> v[100001];
+	int* visit = (int*)malloc(sizeof(int) * (n + 1));
+	for (int i = 0; i < n + 1; i++) visit[i] = 0;
+	int cnt = 1;
+
+	int a, b;
+	for (int i = 0; i < m; i++) {
+		cin >> a >> b;
+		v[a].push_back(b);
+		v[b].push_back(a);
+	}
+	for (int i = 0; i < n + 1; i++) sort(v[i].begin(), v[i].end(), compare);
+	DFS(v, visit, r, &cnt);
+
+	for (int i = 1; i < n + 1; i++) cout << visit[i] << "\n";
+
+	free(visit);
+	return 0;
+}
