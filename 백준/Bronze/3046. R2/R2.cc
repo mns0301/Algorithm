2794c9cd7f9fe7c7af0d4b36c3f873eec91aba21
mns0301 <unitymns@gmail.com>
2024-09-05 17:10:01 +0900

    [Bronze IV] Title: R2, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/3046. R2/R2.cc
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int main(void) {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	cout.tie(0);
+
+	int n;
+	cin >> n;
+	int t;
+	cin >> t;
+
+	cout << 2 * t - n;	
+	return 0;
+}
