ba764cf5ce26558ad0ab47b5a5f6d80c735550da
mns0301 <unitymns@gmail.com>
2024-09-08 21:53:16 +0900

    [Platinum III] Title: 님 게임 나누기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Platinum/11872. 님 게임 나누기/님 게임 나누기.cc
+++ 백준/Platinum/11872. 님 게임 나누기/님 게임 나누기.cc
@@ -1,22 +1,2 @@
-#include <iostream>
-using namespace std;
-
-int main(void) {
-	ios::sync_with_stdio(0);
-	cin.tie(0);
-	cout.tie(0);
-
-	int n;
-	cin >> n;
-	int t;
-	int ans = 0;
-	for (int i = 0; i < n; i++) {
-		cin >> t;
-		if (t % 4 == 3) ans ^= t + 1;
-		else if (t % 4 == 0) ans ^= t - 1;
-		else ans ^= t;
-	}
-	if (ans) cout << "koosaga";
-	else cout << "cubelover";
-	return 0;
-}
+#include<iostream>
+using namespace std;int n,t,a,i;main(){cin>>n;while(n--){cin>>t;if(t%4==3)a^=t+1;else if(t%4==0)a^=t-1;else a^=t;}if(a)cout<<"koosaga";else cout<<"cubelover";}
