1d25eba046fad806b307a89d7ebec67062843260
mns0301 <unitymns@gmail.com>
2024-10-01 09:36:18 +0900

    [Gold III] Title: 약수 지우기 게임 1, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Gold/12107. 약수 지우기 게임 1/약수 지우기 게임 1.cc
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+#define ll long long
+#define vi vector<int>
+#define vl vector<ll>
+#define pii pair<int, int>
+#define pll pair<ll, ll>
+#define FIO cin.tie(nullptr); cout.tie(nullptr); ios_base::sync_with_stdio(false);
+
+int main(void) {
+	FIO;
+
+	int n;
+	cin >> n;
+	cout << (n == 1 ? "B" : "A");
+
+	return 0;
+}
