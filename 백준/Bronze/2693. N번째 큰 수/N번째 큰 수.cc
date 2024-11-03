fcedb950aa64d69583ff9bdd5ba278e8df73fe6b
mns0301 <unitymns@gmail.com>
2024-11-04 08:59:00 +0900

    [Bronze I] Title: N번째 큰 수, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2693. N번째 큰 수/N번째 큰 수.cc
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <array>
+#include <deque>
+#include <queue>
+#include <map>
+#include <cmath>
+using namespace std;
+
+#define INT_MAX 2147483647
+#define INT_MIN -2147483648
+#define fastio ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0)
+#define ll long long
+#define str string
+#define vi vector<int>
+#define vl vector<ll>
+#define vvi vector<vi>
+#define vvl vector<vl>
+#define pii pair<int, int>
+#define pll pair<ll, ll>
+#define vpii vector<pii>
+#define vpll vector<pll>
+#define w(t) while(t--)
+#define f(i, n) for(int i = 0; i < n; i++)
+
+int main() {
+	fastio;
+
+    int t;
+    cin >> t;
+    w(t) {
+        int arr[10] = {};
+        f(i, 10) cin >> arr[i];
+        sort(arr, arr + 10);
+        cout << arr[7] << "\n";
+    }
+
+    return 0;
+}
