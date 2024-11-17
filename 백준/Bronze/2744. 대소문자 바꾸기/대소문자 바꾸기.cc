287f7181f0aff61e01ec7bad84067050d3e4c73b
mns0301 <unitymns@gmail.com>
2024-11-17 23:08:08 +0900

    [Bronze V] Title: 대소문자 바꾸기, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/2744. 대소문자 바꾸기/대소문자 바꾸기.cc
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <array>
+#include <deque>
+#include <queue>
+#include <stack>
+#include <list>
+#include <map>
+#include <set>
+#include <cmath>
+using namespace std;
+
+#define INT_MAX 2147483647
+#define INT_MIN -2147483648
+#define fastio ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0)
+#define ll long long
+#define vi vector<int>
+#define vl vector<ll>
+#define vvi vector<vi>
+#define vvl vector<vl>
+#define pii pair<int, int>
+#define pll pair<ll, ll>
+#define vpii vector<pii>
+#define vpll vector<pll>
+#define w(t) while(t--)
+#define f(i, n) for(int i = 0; i < n; i++)
+
+int main() {
+	fastio;
+
+    string str;
+    cin >> str;
+
+	f(i, str.size()) {
+		if (str[i] >= 'a')
+            str[i] -= 32;
+		else
+            str[i] += 32;
+    }
+
+    cout << str;
+
+    return 0;
+}
