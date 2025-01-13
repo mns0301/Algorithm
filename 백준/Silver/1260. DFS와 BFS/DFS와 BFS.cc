f96f6db3abea81c203139c2b0e8b96669742de50
mns0301 <unitymns@gmail.com>
2025-01-13 15:41:38 +0900

    [Silver II] Title: DFS와 BFS, Time: 0 ms, Memory: 2288 KB -BaekjoonHub

--- 백준/Silver/1260. DFS와 BFS/DFS와 BFS.cc
+++ 백준/Silver/1260. DFS와 BFS/DFS와 BFS.cc
@@ -1,62 +1,109 @@
 #include <iostream>
 #include <algorithm>
-#include <queue>
 #include <vector>
+#include <array>
+#include <deque>
+#include <queue>
+#include <stack>
+#include <list>
+#include <map>
+#include <set>
+#include <cmath>
 using namespace std;
 
-void BFS(vector<int> v[], int visit[], int n, int cnt, vector<int>& v1) {
-	queue<int> que;
-	que.push(n);
-	int temp = 0;
-	while (!que.empty()) {
-		temp = que.front();
-		que.pop();
-		if (visit[temp] == 0) {
-			visit[temp] = cnt++;
-			v1.push_back(temp);
-		}
-		else continue;
+#define fastio ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0)
+#define ll long long
+#define vi vector<int>
+#define vl vector<ll>
+#define vvi vector<vi>
+#define vvl vector<vl>
+#define pii pair<int, int>
+#define pll pair<ll, ll>
+#define vpii vector<pii>
+#define vpll vector<pll>
+#define w(t) while(t--)
+#define f(i, n) for(int i = 0; i < n; i++)
+#define f1(i, n) for(int i = 1; i <= n; i++)
 
-		for (int i = 0; i < v[temp].size(); i++) que.push(v[temp][i]);
-	}
-	return;
+int dy[8] = { -1, -1, -1, 0, 1, 1, 1, 0 };
+int dx[8] = { -1, 0, 1, 1, 1, 0, -1, -1 };
+
+void dfs(vi graph[], int v, int n) {
+    int cnt = 1;
+    vi visited(n + 1, 0);
+    stack<int> st;
+    st.push(v);
+
+    while (!st.empty()) {
+        int now = st.top();
+        st.pop();
+
+        if (visited[now] == 1) continue;
+        else {
+            cout << now << " ";
+            visited[now] = 1;
+        }
+
+        for (int i = graph[now].size() - 1; i >= 0; i--) {
+            int nxt = graph[now][i];
+            if (visited[nxt] == 0)
+                st.push(nxt);
+        }
+    }
+
+    cout << "\n";
 }
 
-void DFS(vector<int> v[], int visit[], int n, int& cnt, vector<int>& v2) {
-	if (visit[n] == 0) {
-		visit[n] = cnt++;
-		v2.push_back(n);
-		for (int i = 0; i < v[n].size(); i++) DFS(v, visit, v[n][i], cnt, v2);
-	}
-	return;
+void bfs(vi graph[], int v, int n)
+{
+    int cnt = 1;
+    vi visited(n + 1, 0);
+    queue<int> qu;
+    qu.push(v);
+
+    while (!qu.empty()) {
+        int now = qu.front();
+        qu.pop();
+
+        if (visited[now] == 1)
+            continue;
+        else {
+            cout << now << " ";
+            visited[now] = 1;
+        }
+
+        for (int i = 0; i < graph[now].size(); i++)
+        {
+            int nxt = graph[now][i];
+            if (visited[nxt] == 0)
+                qu.push(nxt);
+        }
+    }
+
+    cout << "\n";
 }
 
-int main(void) {
-	int n, m, s;
-	cin >> n >> m >> s;
-	int a, b;
-	vector<int> v[1001];
-	int* BFS_visit = new int[n + 1]();
-	int* DFS_visit = new int[n + 1]();
+int main() {
+    fastio;
 
-	for (int i = 0; i < m; i++) {
-		cin >> a >> b;
-		v[a].push_back(b);
-		v[b].push_back(a);
-	}
+    int n, m, v;
+    cin >> n >> m >> v;
+    int a[10];
 
-	for (int i = 1; i < n + 1; i++) sort(v[i].begin(), v[i].end());
-	int cnt = 1;
-	vector<int> D;
-	vector<int> B;
-	DFS(v, DFS_visit, s, cnt, D);
-	BFS(v, BFS_visit, s, 1, B);
+    vi graph[n + 1];
 
-	for (int i = 0; i < D.size(); i++) cout << D[i] << " ";
-	cout << "\n";
-	for (int i = 0; i < B.size(); i++) cout << B[i] << " ";
+    for (int i = 0; i < m; i++) {
+        int x, y;
+        cin >> x >> y;
+        graph[x].push_back(y);
+        graph[y].push_back(x);
+    }
 
-	delete[] BFS_visit;
-	delete[] DFS_visit;
-	return 0;
-}	
+    for (int i = 1; i <= n; i++) 
+        sort(graph[i].begin(), graph[i].end());
+
+    dfs(graph, v, n);
+    bfs(graph, v, n);
+
+    return 0;
+}
