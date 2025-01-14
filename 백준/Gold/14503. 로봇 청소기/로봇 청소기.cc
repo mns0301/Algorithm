05c36c36b0fb07e87436ceed0d614d2070afd986
mns0301 <unitymns@gmail.com>
2025-01-14 11:24:46 +0900

    [Gold V] Title: 로봇 청소기, Time: 0 ms, Memory: 2152 KB -BaekjoonHub

--- 백준/Gold/14503. 로봇 청소기/로봇 청소기.cc
+++ 백준/Gold/14503. 로봇 청소기/로봇 청소기.cc
@@ -1,32 +1,9 @@
 #include <iostream>
 #include <algorithm>
 #include <vector>
-#include <array>
-#include <deque>
-#include <queue>
-#include <stack>
-#include <list>
-#include <map>
-#include <set>
-#include <cmath>
 using namespace std;
 
 #define fastio ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0)
-#define ll long long
-#define vi vector<int>
-#define vl vector<ll>
-#define vvi vector<vi>
-#define vvl vector<vl>
-#define pii pair<int, int>
-#define pll pair<ll, ll>
-#define vpii vector<pii>
-#define vpll vector<pll>
-#define w(t) while(t--)
-#define f(i, n) for(int i = 0; i < n; i++)
-#define f1(i, n) for(int i = 1; i <= n; i++)
-
-int dy[8] = { -1, -1, -1, 0, 1, 1, 1, 0 };
-int dx[8] = { -1, 0, 1, 1, 1, 0, -1, -1 };
 
 int rotate_direction(int dir) {
     if (dir == 0)
