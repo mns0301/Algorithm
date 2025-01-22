4833cbe0a41e0568ec0618193c8b896b7b431cb1
mns0301 <unitymns@gmail.com>
2025-01-22 09:59:40 +0900

    [Gold V] Title: 강의실, Time: 40 ms, Memory: 3704 KB -BaekjoonHub

--- 백준/Gold/1374. 강의실/강의실.cc
+++ 백준/Gold/1374. 강의실/강의실.cc
@@ -6,17 +6,11 @@ using namespace std;
 
 #define fastio ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0)
 
-int dy8[8] = { -1, -1, -1, 0, 1, 1, 1, 0 };
-int dx8[8] = { -1, 0, 1, 1, 1, 0, -1, -1 };
-int dy4[4] = { -1, 0, 1, 0 };
-int dx4[4] = { 0, 1, 0, -1 };
-
 int main() {
     fastio;
 
     int n;
     cin >> n;
-    int cls = 0;
     vector<pair<int, int>> v(n);
     for (int i = 0; i < n; i++) {
         int x, y, z;
@@ -24,18 +18,13 @@ int main() {
         v[i] = { y, z };
     }
     sort(v.begin(), v.end());
-    priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
-    pq.push({ v[0].second, 0 });
+    priority_queue<int> pq;
+    pq.push(-v[0].second);
 
     for (int i = 1; i < n; i++) {
-        auto [end, c] = pq.top();
-        if (end <= v[i].first) {
+        if (-pq.top() <= v[i].first)
             pq.pop();
-            pq.push({ v[i].second, c });
-        }
-        else {
-            pq.push({ v[i].second, ++cls });
-        }
+        pq.push(-v[i].second);
     }
 
     cout << pq.size();
