4b1bfc9dbe7cb8a326c1917550dab7838f22aa7c
mns0301 <unitymns@gmail.com>
2025-01-25 19:47:04 +0900

    [Gold II] Title: 물대기, Time: 12 ms, Memory: 3904 KB -BaekjoonHub

---
+++ 백준/Gold/1368. 물대기/물대기.cc
@@ -0,0 +1,79 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <queue>
+using namespace std;
+
+#define fastio ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0)
+
+int main() {
+	fastio;
+
+	int n;
+	cin >> n;
+
+	vector<int> wi(n), visited(n);
+	vector<vector<int>> w(n, vector<int>(n)), graph(n);
+	int ans = 0;
+	priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
+
+	for (int i = 0; i < n; i++) {
+		cin >> wi[i];
+		pq.push({wi[i], i});
+	}
+		
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < n; j++)
+			cin >> w[i][j];
+	}
+
+	for (int i = 0; i < n; i++) {
+		// int flag = 1;
+		for (int j = 0; j < n; j++) {
+			if (i==j) continue;
+			
+			graph[i].push_back(j);
+			// if (wi[i] > w[i][j]) {
+			// 	flag = 0;
+			// }
+		}
+		// if (flag) 
+		// 	pq.push({wi[i], i});
+	}
+
+	// if (pq.empty()) {
+	// 	int idx = 0;
+	// 	int m = wi[0];
+	// 	for (int i = 1; i < n; i++) {
+	// 		if (wi[i] < m) {
+	// 			idx = i;
+	// 			m = wi[i];
+	// 		}
+	// 	}
+	// 	pq.push({m, idx});
+	// }
+
+	while (pq.size())
+	{
+		int weight = pq.top().first;
+		int now = pq.top().second;
+		pq.pop();
+
+		if (visited[now])
+			continue;
+
+		visited[now] = 1;
+		ans += weight;
+		for (int nxt : graph[now])
+		{
+			if (visited[nxt] == 1)
+				continue;
+
+			pq.push({w[now][nxt], nxt});
+		}
+	}
+
+	cout << ans;
+
+	return 0;
+}
