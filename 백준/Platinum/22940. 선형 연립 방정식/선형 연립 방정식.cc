344b6788c85f2fbab5ec518de804cd14721058d0
mns0301 <unitymns@gmail.com>
2025-03-10 16:32:33 +0900

    [Platinum V] Title: 선형 연립 방정식, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Platinum/22940. 선형 연립 방정식/선형 연립 방정식.cc
@@ -0,0 +1,69 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <cmath>
+using namespace std;
+
+#define fastio ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0)
+
+vector<vector<double>> a(6, vector<double>(7));
+vector<double> ans(6);
+int n;
+
+void printing() {
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j <= n; j++)
+            cout << a[i][j] << " ";
+        cout << "\n";
+    }
+    cout << "\n";
+}
+
+void pivoting(int i) {
+    int j = i;
+    for (j; j < n; j++) {
+        if (a[j][i] != 0) 
+            break;
+    }
+
+    vector<double> tmp = a[i];
+    a[i] = a[j];
+    a[j] = tmp;
+} 
+
+int main() {
+    fastio;
+
+    cin >> n;
+
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j <= n; j++)
+            cin >> a[i][j];
+    }
+
+    for (int idx = 0; idx < n - 1; idx++) {
+        if (a[idx][idx] == 0)
+            pivoting(idx);
+
+        for (int t = idx + 1; t < n; t++) {
+            double mul = a[t][idx] / a[idx][idx];
+            for (int idx2 = idx; idx2 <= n; idx2++) {
+                a[t][idx2] -= a[idx][idx2] * mul;
+            }
+        }
+
+        // printing();
+    }
+
+    for (int i = n-1; i >= 0; i--) {
+        for (int j = i + 1; j < n; j++) {
+            a[i][n] -= a[i][j] * ans[j];
+        }
+        ans[i] = a[i][n] / a[i][i];
+    }
+    for (int i = 0; i < n; i++) {
+        cout << ans[i] << " ";
+    }
+
+        return 0;
+}
