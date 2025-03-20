ff35028cab6c9f82c445c7f20945cceedd4e5ae5
mns0301 <unitymns@gmail.com>
2025-03-20 20:58:07 +0900

    [Gold I] Title: 부분수열의 합 2, Time: 24 ms, Memory: 53500 KB -BaekjoonHub

--- 백준/Gold/1208. 부분수열의 합 2/부분수열의 합 2.cc
+++ 백준/Gold/1208. 부분수열의 합 2/부분수열의 합 2.cc
@@ -14,17 +14,28 @@ using namespace std;
 #define fastio ios_base::sync_with_stdio(false); cin.tie(nullptr); cout.tie(nullptr)
 #define ll long long
 
-vector<ll> v, v1, v2;
+vector<ll> v, v2;
+ll a1[5000001];
 int n;
 ll s;
 ll ans = 0;
-void fun0(vector<ll> &tmp, int st, int en, ll sum) {
+void fun0(ll *tmp, int st, int en, ll sum) {
     if (st == en) {
-        tmp.push_back(sum);
+        tmp[sum+2500000]++;
         return;
     }
     fun0(tmp, st + 1, en, sum + v[st]);
     fun0(tmp, st + 1, en, sum);
+}
+void fun1(vector<ll> &tmp, int st, int en, ll sum)
+{
+    if (st == en)
+    {
+        tmp.push_back(sum);
+        return;
+    }
+    fun1(tmp, st + 1, en, sum + v[st]);
+    fun1(tmp, st + 1, en, sum);
 }
 
 int main() {
@@ -37,12 +48,13 @@ int main() {
         v.push_back(t);
     }
 
-    fun0(v1, 0, (n + 1) / 2, 0);
-    sort(v1.begin(), v1.end());
-    fun0(v2, (n + 1) / 2, n, 0);
+    fun0(a1, 0, (n + 1) / 2, 0);
+    fun1(v2, (n + 1) / 2, n, 0);
 
-    for (auto i : v2) 
-        ans += upper_bound(v1.begin(), v1.end(), s - i) - lower_bound(v1.begin(), v1.end(), s - i);
+    for (auto i : v2) {
+        if (s - i <= 2500000 && 0 <= 2500000 + s - i)
+            ans += a1[2500000 + s - i];
+    }
 
     if (s==0)
         ans--;
