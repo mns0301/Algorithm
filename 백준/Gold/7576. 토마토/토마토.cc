dd3c180d695eecc7173c7b8e54d06fa158f44e44
mns0301 <unitymns@gmail.com>
2025-05-15 20:13:21 +0900

    [Gold V] Title: 토마토, Time: 76 ms, Memory: 12504 KB -BaekjoonHub

--- 백준/Gold/7576. 토마토/토마토.cc
+++ 백준/Gold/7576. 토마토/토마토.cc
@@ -1,67 +1,68 @@
 #include <iostream>
 #include <vector>
+#include <array>
 #include <queue>
 using namespace std;
 
-int BFS(vector<vector<int>>& v, queue<pair<int, int>>& pos, int y, int x, int n, int m) {
-	if (x < 0 || y < 0 || x == m || y == n) return 0;
-	if (v[y][x] == 0) {
-		v[y][x] = 1;
-		pos.push(make_pair(y, x));
-		return 1;
-	}
-	return 0;
-}
+#define fastio ios_base::sync_with_stdio(false); cin.tie(nullptr); cout.tie(nullptr)
+#define vi vector<int>
+#define vvi vector<vi>
+#define ai3 array<int, 3>
+#define qai3 queue<ai3>
+#define w(t) while(t)
+#define f1(i, n) for(int i = 1; i <= n; i++)
+#define defvari2(a, b) int a, b;
+#define defvari3(a, b, c) int a, b, c;
+#define inputi2(n, m) cin >> n >> m;
+#define inputi1(n) cin >> n;
+#define outputi1(n) cout << n;
+#define pushai3(a, b, c) push({ a, b, c });
+#define matchvii2(a, b, c, d) a = c, b = d;
+#define matchvpi3(a, b, c, d) a = d[0], b = d[1], c = d[2];
+int dy[4] = { -1, 0, 1, 0 };
+int dx[4] = { 0, 1, 0, -1 };
 
-int main(void) {
-	ios::sync_with_stdio(0);
-	cin.tie();
-	cout.tie();
+defvari3(n, m, a)
 
-	int n, m;
-	cin >> m >> n;
-	int day = -1;
-	int changed = 0;
-	vector<vector<int>> v(n, vector<int>(m, 0));
-	queue<pair<int, int>> pos;
-	
-	for (int i = 0; i < n; i++) {
-		for (int j = 0; j < m; j++) {
-			cin >> v[i][j];
-			if (v[i][j] == 1) {
-				pos.push(make_pair(i, j));
-				changed++;
-			}
-		}
-	}
+int main() {
+    fastio;
 
-	int x = 0, y = 0, temp = 0;
-	while (changed != 0) {
-		temp = 0;
-		for (int i = 0; i < changed; i++) {
-			y = pos.front().first;
-			x = pos.front().second;
-			pos.pop();
-			temp += BFS(v, pos, y - 1, x, n, m);
-			temp += BFS(v, pos, y, x - 1, n, m);
-			temp += BFS(v, pos, y + 1, x, n, m);
-			temp += BFS(v, pos, y, x + 1, n, m);
-		}
-		day++;
-		changed = temp;
-	}
+    inputi2(m, n)
+    vvi v(n + 2, vi(m + 2, -1));
+    qai3 q;
 
-	for (int i = 0; i < n; i++) {
-		for (int j = 0; j < m; j++) {
-			//cout << v[i][j] << " ";
-			if (v[i][j] == 0) {
-				cout << -1;
-				return 0;
-			}
-		}
-		//cout << "\n";	
-	}
-	cout << day;
+    f1(i, n) {
+        f1(j, m)  {
+            inputi1(v[i][j]);
+            if (v[i][j] == 1)
+                q.pushai3(i, j, 0);
+        }
+    }
 
-	return 0;
+    w(q.size()) {
+        defvari3(y, x, d);
+        matchvpi3(y, x, d, q.front());
+        q.pop();
+
+        a = d++;
+        for (int i = 0; i < 4; i++) {
+            defvari2(ny, nx);
+            matchvii2(ny, nx, y + dy[i], x + dx[i]);
+            if (!v[ny][nx]) {
+                v[ny][nx] = 1;
+                q.pushai3(ny, nx, d);
+            }
+        }
+    }
+
+    f1(i, n) {
+        f1(j, m) {
+            if (!v[i][j])
+                a = -1;
+        }
+    }
+
+    outputi1(a);
+
+    return 0;
 }
