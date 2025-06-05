9ef7612b278a295301307105f2a37ed379dbf354
mns0301 <unitymns@gmail.com>
2025-06-05 11:09:14 +0900

    [Silver IV] Title: 보물, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Silver/1026. 보물/보물.cc
@@ -0,0 +1,53 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <array>
+#include <deque>
+#include <queue>
+#include <stack>
+#include <list>
+#include <map>
+#include <set>
+#include <cmath>
+using namespace std;
+
+#define fastio ios_base::sync_with_stdio(false); cin.tie(nullptr); cout.tie(nullptr)
+#define ll long long
+#define vi vector<int>
+#define vl vector<ll>
+#define vvi vector<vi>
+#define vvl vector<vl>
+#define pii pair<int, int>
+#define pll pair<ll, ll>
+#define vpii vector<pii>
+#define vpll vector<pll>
+#define w(t) while(t--)
+#define f(i, n) for(int i = 0; i < n; i++)
+#define f1(i, n) for(int i = 1; i <= n; i++)
+#define unique(v) v.erase(unique(v.begin(), v.end()), v.end());
+#define max(a,b) (a > b ? a : b)
+#define min(a,b) (a < b ? a : b)
+
+int dy8[8] = { -1, -1, -1, 0, 1, 1, 1, 0 };
+int dx8[8] = { -1, 0, 1, 1, 1, 0, -1, -1 };
+int dy4[4] = { -1, 0, 1, 0 };
+int dx4[4] = { 0, 1, 0, -1 };
+
+int main() {
+    fastio;
+
+    int n;
+    cin >> n;
+    vi a(n), b(n);
+    f(i, n) cin >> a[i];
+    f(i, n) cin >> b[i];
+    sort(a.begin(), a.end());
+    sort(b.begin(), b.end());
+    int c = 0;
+    f(i, n) {
+        c += a[i] * b[n - 1 - i];
+    }
+    cout << c;  
+
+    return 0;
+}
