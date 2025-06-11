0ef19f927d401bbdcbaf9d2e1b8fd631ac31065c
mns0301 <unitymns@gmail.com>
2025-06-11 11:52:39 +0900

    [Bronze V] Title: 정보보호학부 동아리 소개, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/28691. 정보보호학부 동아리 소개/정보보호학부 동아리 소개.py
@@ -0,0 +1,12 @@
+c = input()
+if (c == 'M'):
+    s ="MatKor"
+if (c == 'W'):
+    s ="WiCys"
+if (c == 'C'):
+    s ="CyKor"
+if (c == 'A'):
+    s ="AlKor"
+if (c == '$'):
+    s ="$clear"
+print(s)
