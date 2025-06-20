ecba8b2c1cc315338252edfd2dbd93cc47b78aa7
mns0301 <unitymns@gmail.com>
2025-06-20 15:37:03 +0900

    [Silver V] Title: Costume Party, Time: 80 ms, Memory: 2220 KB -BaekjoonHub

---
+++ 백준/Silver/6159. Costume Party/Costume Party.cc
@@ -0,0 +1,59 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <array>
+#include <deque>
+#include <queue>
+#include <stack>
+#include <list>
+#include <map>
+#include <set>
+#include <cmath>
+using namespace std;
+
+#define fastio ios_base::sync_with_stdio(false); cin.tie(nullptr); cout.tie(nullptr)
+#define ll long long
+#define vi vector<int>
+#define vl vector<ll>
+#define vvi vector<vi>
+#define vvl vector<vl>
+#define pii pair<int, int>
+#define pll pair<ll, ll>
+#define vpii vector<pii>
+#define vpll vector<pll>
+#define w(t) while(t--)
+#define f(i, n) for(int i = 0; i < n; i++)
+#define f1(i, n) for(int i = 1; i <= n; i++)
+#define fe(c, v) for(auto c : v)
+#define unique(v) v.erase(unique(v.begin(), v.end()), v.end());
+#define max(a,b) (a > b ? a : b)
+#define min(a,b) (a < b ? a : b)
+#define x
+
+int dy8[8] = { -1, -1, -1, 0, 1, 1, 1, 0 };
+int dx8[8] = { -1, 0, 1, 1, 1, 0, -1, -1 };
+int dy4[4] = { -1, 0, 1, 0 };
+int dx4[4] = { 0, 1, 0, -1 };
+
+int main() {
+    fastio;
+
+    int n, s;
+    cin >> n >> s;
+    vi v(n);
+    f(i, n) cin >> v[i];
+    sort(v.begin(), v.end());
+
+    ll ans = 0;
+    f(i, n) {
+        for (int j = i + 1; j < n; j++) {
+            if (v[i] + v[j] <= s)
+                ans++;
+            else
+                break;
+        }
+    }
+        cout << ans;
+
+    return 0;
+}
