b200a13f72a590818efc430cb7495af22412bf73
mns0301 <unitymns@gmail.com>
2025-09-07 21:12:45 +0900

    [Silver III] Title: 파도반 수열, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/9461. 파도반 수열/파도반 수열.cc
+++ 백준/Silver/9461. 파도반 수열/파도반 수열.cc
@@ -1,2 +1,2 @@
-#include<iostream>
-long d[100]={1,1,1},t,n;main(){for(t;t<97;)d[t+2]=d[t++]+d[t];std::cin>>t;while(t--){std::cin>>n;std::cout<<d[n-1]<<"\n";}}
+#import<iostream>
+long d[101]={1,1,1},i;main(){for(i;i<97;)d[i+2]=d[i++]+d[i];for(std::cin>>i;std::cin>>i;)printf("%ld ",d[i-1]);}
