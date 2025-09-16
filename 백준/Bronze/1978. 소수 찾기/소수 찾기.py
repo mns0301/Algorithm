ac4a8d1cb7e902fd1da926f516c815b448a5f90b
mns0301 <unitymns@gmail.com>
2025-09-16 14:17:34 +0900

    [Bronze II] Title: 소수 찾기, Time: 88 ms, Memory: 109544 KB -BaekjoonHub

--- 백준/Bronze/1978. 소수 찾기/소수 찾기.py
+++ 백준/Bronze/1978. 소수 찾기/소수 찾기.py
@@ -1,11 +1,13 @@
-n = int(input())
-a = list(map(int, input().split()))
-ans = 0
-for i in a:
-	flag = 0
-	if (i == 1) : continue
-	for j in range(2, i):
-		if (i%j ==0): flag = 1
-	if (flag == 0) :
-		ans+=1
-print(ans)
+a = int(input())
+b = map(int, input().split())
+c = 0
+for i in b :
+	if (i == 1) : 
+		continue
+	for j in range(2, i) :
+		if i%j == 0:
+			break
+	else :
+		c += 1
+	
+print(c)
