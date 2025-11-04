52b5510354fabe1736a981460cb0cb099f02d4b1
mns0301 <unitymns@gmail.com>
2025-11-04 18:01:47 +0900

    [Bronze I] Title: 소가 길을 건너간 이유 1, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/14467. 소가 길을 건너간 이유 1/소가 길을 건너간 이유 1.cc
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define fastio                        \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(nullptr);                 \
+    cout.tie(nullptr)
+// #define int long long
+#define ll long long
+#define ld long double
+#define pii pair<int, int>
+#define vi vector<int>
+#define vvi vector<vi>
+#define vpii vector<pii>
+#define loop(t) while (t--)
+#define f(i, n) for (int i = 0; i < n; i++)
+#define f1(i, n) for (int i = 1; i <= n; i++)
+#define foreach(i, v) for (auto i : v)
+#define unique(v) v.erase(unique(v.begin(), v.end()), v.end());
+
+int dy8[8] = { -1, -1, -1, 1, 1, 1, 0, 0 };
+int dx8[8] = { -1, 0, 1, -1, 0, 1, -1, 1 };
+int dy4[4] = { 0, 0, -1, 1 };
+int dx4[4] = { -1, 1, 0, 0 };
+
+int main() {
+    fastio;
+
+    int n, ans = 0;
+    cin >> n;
+    vi cow(11, -1);
+    f(i, n) {
+        int x, p;
+        cin >> x >> p;
+        if (cow[x] == -1)
+            cow[x] = p;
+        if (cow[x] != p) {
+            ans++;
+            cow[x] = p;
+        }
+    }
+    cout << ans;
+    return 0;
+}
