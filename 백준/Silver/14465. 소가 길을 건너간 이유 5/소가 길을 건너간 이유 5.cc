6f21db9c77caf47bd3498512e8352ce2d4e32280
mns0301 <unitymns@gmail.com>
2025-11-04 17:53:53 +0900

    [Silver II] Title: 소가 길을 건너간 이유 5, Time: 0 ms, Memory: 2412 KB -BaekjoonHub

---
+++ 백준/Silver/14465. 소가 길을 건너간 이유 5/소가 길을 건너간 이유 5.cc
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define fastio ios_base::sync_with_stdio(false); cin.tie(nullptr); cout.tie(nullptr)
+//#define int long long
+#define ll long long
+#define ld long double
+#define pii pair<int, int>
+#define vi vector<int>
+#define vvi vector<vi>
+#define vpii vector<pii>
+#define loop(t) while(t--)
+#define f(i, n) for(int i = 0; i < n; i++)
+#define f1(i, n) for(int i = 1; i <= n; i++)
+#define foreach(i, v) for(auto i : v)
+#define unique(v) v.erase(unique(v.begin(), v.end()), v.end());
+
+int dy8[8] = { -1, -1, -1, 1, 1, 1, 0, 0 };
+int dx8[8] = { -1, 0, 1, -1, 0, 1, -1, 1 };
+int dy4[4] = { 0, 0, -1, 1 };
+int dx4[4] = { -1, 1, 0, 0 };
+
+int main() {
+	fastio;
+
+    int n, k, b;
+    cin >> n >> k >> b;
+    vi v(n + 1, 0);
+	f(i, n) {
+        int t;
+        cin >> t;
+        v[t] = 1;
+    }
+    int cur = 0;
+    f1(i, k) cur += v[i];
+    int fix = cur;
+    for (int i = k + 1; i <= n; i++) {
+        cur += v[i] - v[i - k];
+        fix = min(cur, fix);
+    }
+    cout << fix;
+
+    return 0;
+}
